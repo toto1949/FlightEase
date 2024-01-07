31b62b4a3df9b08a7682143de8b228c72743be40
Taoufiq <rztaoufiq@gmail.com>
2024-01-06 22:27:07 -0600

    the first commit for initial the parent project

---
+++ src/main/java/com/flightease/back/FlightEaseBackApplication.java
@@ -0,0 +1,13 @@
+package com.flightease.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightEaseBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightEaseBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flightease/back/FlightEaseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightease.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightEaseBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
