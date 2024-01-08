ac82dc1c5a95e4f3ee8413b7d4e8a214af480253
Taoufiq <rztaoufiq@gmail.com>
2024-01-08 15:04:44 -0600

    update config

--- authentication-service/src/main/java/com/taoufiq/authentication/service/UserService.java
+++ authentication-service/src/main/java/com/taoufiq/authentication/service/UserService.java
@@ -3,7 +3,6 @@ package com.taoufiq.authentication.service;
 import com.taoufiq.authentication.entities.User;
 import com.taoufiq.authentication.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -13,8 +12,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    private PasswordEncoder passwordEncoder;
-
 
     public User registerUser(User user) {
         if (userRepository.findByUsername(user.getUsername()) != null ||
@@ -22,11 +19,6 @@ public class UserService {
             return null;
         }
         user.setPassword(user.getPassword());
-
-        // Assign default role or roles if needed
-        // Set<String> roles = new HashSet<>(Arrays.asList("ROLE_USER"));
-        // user.setRoles(roles);
-
         return userRepository.save(user);
     }
 
